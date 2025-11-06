a6a8595fac8386752bce1832c3836343e28f317b
paradoxical-dev <jaredonnell21@gmail.com>
2025-11-06 11:54:12 -0600

    feat(minty): added minty color picker

---
+++ lua/plugins/minty.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"nvzone/minty",
+		dependencies = { "nvzone/volt" },
+		lazy = true,
+		keys = {
+			{
+				"<leader>S",
+				"<cmd>Shades<cr>",
+				desc = "Shades Color Picker",
+			},
+			{
+				"<leader>H",
+				"<cmd>Huefy<cr>",
+				desc = "Huefy Color Picker",
+			},
+		},
+	},
+}
