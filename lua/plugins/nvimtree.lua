3cc2404d6ee5dfa90521b104186c6d419da65430
paradoxical-dev <jaredonnell21@gmail.com>
2025-11-06 10:25:51 -0600

    feat(neotree): updated config

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -27,8 +27,24 @@ return {
 		},
 		opts = {
 			on_attach = my_on_attach,
+			sync_root_with_cwd = true,
 			view = {
 				width = 35,
+			},
+			tab = {
+				sync = {
+					open = true,
+					close = true,
+				},
+			},
+			actions = {
+				change_dir = {
+					enable = true,
+					global = true,
+				},
+			},
+			update_focused_file = {
+				enable = true,
 			},
 			renderer = {
 				icons = {
