d767f27a733832556f829ddc751c99e79d5ba4da
paradoxical-dev <jaredonnell21@gmail.com>
2026-05-15 17:05:04 -0500

    refactor: moved navigation plugins

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,4 +1,50 @@
+-- INFO: includes packages to imrove navigation and text manipulation
+-- plugins included:
+-- flash.nvim
+-- replace.nvim
+-- nvim-surround
+-- snipe.nvim
+-- harpoon
+
 return {
+	{
+		"chrisgrieser/nvim-rip-substitute",
+		cmd = "RipSubstitute",
+		opts = {},
+		keys = {
+			{
+				"<leader>/",
+				function()
+					require("rip-substitute").sub()
+				end,
+				mode = { "n", "x" },
+				desc = "Search and Replace",
+			},
+		},
+	},
+
+	{
+		"leath-dub/snipe.nvim",
+		keys = {
+			{
+				"<leader>bs",
+				function()
+					require("snipe").open_buffer_menu()
+				end,
+				desc = "Open Snipe",
+			},
+		},
+		opts = {
+			ui = {
+				max_height = 13,
+				open_win_override = {
+					border = "rounded",
+				},
+				position = "bottomright",
+			},
+		},
+	},
+
 	{
 		"ThePrimeagen/harpoon",
 		dependencies = {
